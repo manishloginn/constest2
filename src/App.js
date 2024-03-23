6b68a6a3bae6318c8bfe77a8be9d0231902cad28
manishloginn <manishv7714@gmail.com>
2024-03-23 22:05:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./Login.js";
+import Profile from "./Profile.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  return <>{user === null ? <Login setUser={setUser} /> : <Profile />}</>;
 }
 
 export default App;
